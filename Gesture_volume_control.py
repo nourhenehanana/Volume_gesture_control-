0f07c6eab475105bb029288192c675e913afea2f
Nourhene Hanana <93352403+nourhenehanana@users.noreply.github.com>
2023-10-31 16:32:14 +0100

    Add files via upload

---
+++ Gesture_volume_control.py
@@ -0,0 +1,84 @@
+import cv2
+import mediapipe as mp
+import time
+import numpy as np
+import hand_tracking_2 as htm
+import math
+import pycaw
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+wCam,hCam=648,480
+
+cap=cv2.VideoCapture(0)
+cap.set(3,wCam)
+cap.set(4,hCam)
+pTime=0
+
+#we will create an object from the class handdetector
+detector=htm.handDetector(min_detection_confidence=0.6)
+
+
+
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = interface.QueryInterface(IAudioEndpointVolume)
+
+volRange=volume.GetVolumeRange()
+volume.SetMasterVolumeLevel(0, None)
+minVol=volRange[0]
+maxVol=volRange[1]
+vol=0
+volBar=400
+volPer=0
+while True:
+    success,img=cap.read()
+    img=detector.findHands(img)
+    #get the position
+    lmlist=detector.findPosition(img,draw=False)
+    #we can specify a landmark however we should  make sure that there are points otherwise an error will be displayed
+    #index out of range
+    if len(lmlist)!=0:
+        #print(lmlist[4],lmlist[8])
+        #to know which landmarks to choose we can refer to mediapipe documentation
+        x1,y1=lmlist[4][1],lmlist[4][2]
+        x2,y2=lmlist[8][1],lmlist[8][2]
+        #get the center of the line
+        cx,cy=(x1+x2)//2,(y1+y2)//2
+        cv2.circle(img,(x1,y1),10,(255,0,0),cv2.FILLED)
+        cv2.circle(img,(x2,y2),10,(255,0,0),cv2.FILLED)
+        
+        #create a line between them
+        cv2.line(img,(x1,y1),(x2,y2),(255,0,255),3)
+        cv2.circle(img,(cx,cy),10,(255,0,0),cv2.FILLED)
+        
+        length=math.hypot(x2-x1,y2-y1)
+        #print(length)
+        #Hand range was between 50 and 300
+        #we need to convert the above values to our volume range -65 -0
+        vol=np.interp(length,[50,250],[minVol,maxVol])
+        volBar=np.interp(length,[50,250],[400,150])
+        volPer=np.interp(length,[50,250],[0,100])
+        print(int(length),vol)
+        #now what we will do is that we will give it to the set master volume level
+        volume.SetMasterVolumeLevel(vol, None)
+        
+        if length<50:
+            cv2.circle(img,(cx,cy),10,(255,255,255),cv2.FILLED)
+    
+    cv2.rectangle(img,(50,150),(85,400),(0,255,0),3)
+    #what we will display only the change of height
+    cv2.rectangle(img,(50,int(volBar)),(85,400),(0,255,0),cv2.FILLED)
+    cv2.putText(img, f"Volume: {int(volPer)}%", (40, 450), cv2.FONT_HERSHEY_PLAIN,2, (255,250,0), 3)
+            
+    cTime=time.time()
+    fps=1/(cTime-pTime)
+    pTime=cTime
+    cv2.putText(img, f"FPS: {int(fps)}", (40, 50), cv2.FONT_HERSHEY_PLAIN,2, (255,0,0), 3)
+    cv2.imshow("img",img)
+    key = cv2.waitKey(1) & 0xFF
+    if key==ord("q"):
+        break
+
+cv2.destroyAllWindows()
